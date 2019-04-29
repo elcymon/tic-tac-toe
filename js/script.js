166471adb037e1fea420833d1734eff253525d1c
elcymon <elcymon@gmail.com>
2019-04-29 12:49:48 +0100

    create board structure and javascript mouse over events

---
+++ js/script.js
@@ -0,0 +1,16 @@
+let board = [
+    [-1,-1,-1],
+    [-1,-1,-1],
+    [-1,-1,-1],
+];
+
+let mouseoverFunction = (x) => {
+    x.style.backgroundColor = 'white';
+    // console.log(x);
+}
+
+function mouseoutFunction(x) {
+    x.style.backgroundColor = 'blueviolet';
+    // x.style.borderRadius = '50%';
+    // console.log(x);
+}
