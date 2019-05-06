6e7739725caf45a31a3f4c6473072e5274991873
elcymon <elcymon@gmail.com>
2019-05-06 12:19:31 +0100

    add a bunch of comments

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
+// board layout for the tic-tac-toe
 let board = [
     ['','',''],
     ['','',''],
     ['','',''],
 ];
 
+// players object to hold player1, and player2 
 let players = {
     'p1': 'black',
     'p2': 'yellow',
@@ -11,13 +13,17 @@ let players = {
 };
 
 
+
 let getRowCol = (boxID) => {
+    //return the row and column location of interest based on value of boxID string
+    //box id is of form rRcC, where R and C are from ID of HTML
     let boxRow = parseInt(boxID[1])-1;
     let boxCol = parseInt(boxID[3])-1;
     
     return [boxRow, boxCol];
 }
 let mouseoverFunction = (x) => {
+    //handle behaviour or cell when onmouseover event is triggered
     let boxLoc = getRowCol(x.id);
     if ( board[boxLoc[0]][boxLoc[1]] === '' ){
         x.style.backgroundColor = 'white';
@@ -27,6 +33,7 @@ let mouseoverFunction = (x) => {
 }
 
 function mouseoutFunction(x) {
+    // handle behaviour of cell when onmouseout event is triggered
     let boxLoc = getRowCol(x.id);
     if ( board[boxLoc[0]][boxLoc[1]] === '' ) {
         x.style.backgroundColor = 'blueviolet';
@@ -35,15 +42,17 @@ function mouseoutFunction(x) {
     // console.log(x);
 }
 function changeTurn(info) {
+    // handles change of turn behaviour after a players' turn
     document.getElementById(info.nextPlayer).style.backgroundColor = info.nextPlayer;
     document.getElementById(info.played).style.backgroundColor = 'blueviolet';
 }
 
 let boxDblClick = (x) => {
-    let boxLoc = getRowCol(x.id);
-    if ( board[boxLoc[0]][boxLoc[1]] === '' ) {
+    // handles onclick event for each cell on the board
+    let boxLoc = getRowCol(x.id);//get row and col clicked
+    if ( board[boxLoc[0]][boxLoc[1]] === '' ) {//only trigger clicks on cells that are empty
         played = '';
-        if(players.turn){
+        if(players.turn){//if true, player1 has played its turn
             
             // alert('It is your turn Yellow');
             turnInfo = {
@@ -53,7 +62,7 @@ let boxDblClick = (x) => {
 
 
         }
-        else {
+        else {//handle player2's turn
             
             // alert('It is your turn Black');
             turnInfo = {
@@ -64,13 +73,13 @@ let boxDblClick = (x) => {
         }
 
 
-        players.turn = !(players.turn);
+        players.turn = !(players.turn);//switch player after each turn
         
         x.style.backgroundColor = turnInfo.played;
         board[boxLoc[0]][boxLoc[1]] = turnInfo.played;
 
 
-        changeTurn(turnInfo);
+        changeTurn(turnInfo);//highlight which player's turn it is
     }
     // console.log(board);
 }
